b97f5105291a23e4ba7d32d4b76fe1d90bc27cda
孙雨珩 <sunyuheng@hiynn.com>
2017-08-16 16:56:59 +0800

    add configurable test

---
+++ test/features/configurable/configurable.spec.js
@@ -0,0 +1,147 @@
+import ButBase from 'but-base'
+
+describe('configurable', () => {
+  let component
+  beforeEach(() => {
+    component = new ButBase()
+  })
+
+  it('update', () => {
+    const spy = jasmine.createSpy('updateData')
+
+    component.updateData = spy
+    const data = []
+    component.config({
+      data
+    })
+
+    expect(spy.calls.count()).toBe(1)
+    expect(component.config().data).toBe(data)
+  })
+
+  it('merge', () => {
+    component.config({
+      data: {},
+      normal: {
+        first: 1
+      },
+      arr: []
+    })
+
+    const { normal } = component.config()
+    const data = {}
+    const arr = []
+    component.config({
+      data,
+      normal: {
+        second: 2
+      },
+      arr
+    })
+
+    const config = component.config()
+
+    expect(config.data).toBe(data)
+
+    expect(config.normal).toBe(normal)
+    expect(config.normal.first).toBe(1)
+    expect(config.normal.second).toBe(2)
+
+    expect(config.arr).toBe(arr)
+  })
+
+  it('order', () => {
+    component.getUpdateConfigOrder = function () {
+      return {
+        first: 1,
+        second: 2,
+        third: 3
+      }
+    }
+
+    let order = 1
+
+    component.updateFirst = function () {
+      expect(order).toBe(1)
+      order++
+    }
+
+    component.updateSecond = function () {
+      expect(order).toBe(2)
+      order++
+    }
+
+    component.updateThird = function () {
+      expect(order).toBe(3)
+      order++
+    }
+
+    component.config({
+      third: true,
+      first: true,
+      second: true
+    })
+  })
+
+  it('needExec', () => {
+    component.getUpdateConfigOrder = function () {
+      return {
+        first: 1,
+        second: 2,
+      }
+    }
+
+    let order = 1
+
+    component.updateFirst = function (taskWatier) {
+      taskWatier.needExec('final')
+    }
+
+    component.updateSecond = function () {
+      taskWatier.needExec('final')
+    }
+
+    const spy = jasmine.createSpy('final')
+
+    component.final = spy
+
+    component.config({
+      third: true,
+      first: true,
+    })
+
+    expect(spy.calls.count()).toBe(1)
+  })
+
+  it('needExec during first loop', () => {
+    component.getUpdateConfigOrder = function () {
+      return {
+        first: 1,
+        second: 2,
+        third: 3
+      }
+    }
+
+    let order = 1
+
+    component.updateFirst = function (taskWatier) {
+      taskWatier.needExec('updateThird')
+    }
+
+    component.updateSecond = function () {
+      taskWatier.needExec('updateThird')
+    }
+
+    const spy = jasmine.createSpy('updateThird')
+
+    component.updateThird = spy
+
+    component.config({
+      third: true,
+      first: true,
+      third: true
+    })
+
+    expect(spy.calls.count()).toBe(1)
+  })
+})
