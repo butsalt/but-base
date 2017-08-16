76877bd61bec9737558d2d4ee2630f1105dbe344
孙雨珩 <sunyuheng@hiynn.com>
2017-08-16 15:55:26 +0800

    add test config

--- test/karmaConfig/test.js
+++ test/karmaConfig/test.js
@@ -2,8 +2,13 @@ const base = require('./base')
 
 module.exports = function (config) {
   config.set(
-    Object.assign(base, {
-      
+     Object.assign(base, {
+      browsers: ['Chrome'],
+      reporters: ['progress'],
+      singleRun: true,
+      plugins: base.plugins.concat([
+        'karma-chrome-launcher'
+      ])
     })
   )
 }
