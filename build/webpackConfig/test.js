161a960987e3c8ce4c46a42e545ca2be7f7e3d57
孙雨珩 <sunyuheng@hiynn.com>
2017-08-18 15:26:12 +0800

    improve webpack config for test unit

--- build/webpackConfig/test.js
+++ build/webpackConfig/test.js
@@ -1,19 +1,21 @@
 const merge = require('webpack-merge')
 
-const baseWebpackConfig = require('./base')
+const baseWebpackConfig = Object.assign({}, require('./base'))
 const utils = require('../utils')
 
 module.exports = merge(
   baseWebpackConfig,
   {
+    // entry由karma-webpack管理
+    entry: null,
+    // output由karma-webpack管理
+    output: null,
     resolve: {
       alias: {
+        // 使得可以在单元测试中可以require('but-base')
         'but-base': utils.resolve('src/ButBase.js')
       }
     },
-    devtool: '#cheap-module-eval-source-map',
-    output: {
-      filename: '[name].js',
-    }
+    devtool: '#cheap-module-eval-source-map'
   }
 )
